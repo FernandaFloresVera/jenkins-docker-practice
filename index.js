cd09d6f738cd24ffa507caabc5006b7f9618c1de
FernandaFloresVera <100867517+FernandaFloresVera@users.noreply.github.com>
2024-06-05 20:44:39 +0000

    chido

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hola profe Jesus!');
+    res.send('Hola profe Jesus! :D');
     });
 
 app.listen(port, () => {
